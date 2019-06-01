bf77e2d895ee4ff4db46c9cc4a143b3801910357
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2019-06-01 09:19:51 +0200

    Add intent to create the frequencydetailsactivity

---
+++ app/src/main/java/eu/grelaud/rfdatabase/view/FrequencyDetailsActivity.java
@@ -0,0 +1,13 @@
+package eu.grelaud.rfdatabase.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrequencyDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frequency_details);
+    }
+}
