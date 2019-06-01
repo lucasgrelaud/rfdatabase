2023385ed0f8a425e871deace5e8cd88c44cefaf
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2019-06-01 12:32:25 +0200

    Small refactor to improve readability of data

--- app/src/main/java/eu/grelaud/rfdatabase/controller/FrequencyDetailsController.java
+++ app/src/main/java/eu/grelaud/rfdatabase/controller/FrequencyDetailsController.java
@@ -1,5 +1,7 @@
 package eu.grelaud.rfdatabase.controller;
 
+import android.text.TextUtils;
+import eu.grelaud.rfdatabase.R;
 import eu.grelaud.rfdatabase.model.Frequency;
 import eu.grelaud.rfdatabase.view.FrequencyDetailsActivity;
 
@@ -9,6 +11,10 @@ public class FrequencyDetailsController {
     private FrequencyDetailsActivity frequencyDetailsActivity;
     private Frequency frequency;
     private String frequencyStr;
+    private String type;
+    private String eirp;
+    private String wave_length;
+    private String use;
     private String modeStr;
 
     public FrequencyDetailsController(FrequencyDetailsActivity frequencyDetailsActivity, Frequency frequency) {
@@ -22,16 +28,31 @@ public class FrequencyDetailsController {
             frequencyStr = frequency.getFrequency_range()[0] + " " + frequency.getFrequency_base()
                     + " −" + frequency.getFrequency_range()[1] + " " + frequency.getFrequency_base();
         }
-        StringBuilder modeStrbuilder = new StringBuilder();
-        for (String str : frequency.getTransmition_mode()) {
-            modeStrbuilder.append("▸ ").append(str).append("\n");
+
+        type = validateStringData(frequency.getFrequency_type());
+        eirp = validateStringData(frequency.getEIRP());
+        wave_length = validateStringData(frequency.getWave_length());
+        use = validateStringData(frequency.getUse());
+
+        if (frequency.getTransmition_mode().length != 0 && frequency.getTransmition_mode()[0] != "?") {
+            StringBuilder modeStrbuilder = new StringBuilder();
+            for (String str : frequency.getTransmition_mode()) {
+                modeStrbuilder.append("▸ ").append(str).append("\n");
+            }
+            modeStr = modeStrbuilder.toString();
         }
-        modeStr = modeStrbuilder.toString();
+        else {
+            modeStr = frequencyDetailsActivity.getString(R.string.unknown_data);
+        }
     }
 
     public void start(){
-        frequencyDetailsActivity.updateView(frequencyStr, frequency.getFrequency_type(), frequency.getEIRP(),
-                frequency.getWave_length(), frequency.getUse(), modeStr, frequency.isSpace(), frequency.isEmergency(),
-                Arrays.asList(frequency.getSub_allocation()));
+        frequencyDetailsActivity.updateView(frequencyStr,type, eirp,wave_length, use, modeStr, frequency.isSpace(),
+                frequency.isEmergency(), Arrays.asList(frequency.getSub_allocation()));
+    }
+
+    private String validateStringData(String value){
+        return (!TextUtils.isEmpty(value) && !value.equals("?"))?value:frequencyDetailsActivity
+                .getString(R.string.unknown_data);
     }
 }
